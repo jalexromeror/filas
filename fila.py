e3144727d89fa34ffae7f7a0a1db446b94b43b57
Yimy Romero <jalexromeror@gmail.com>
2022-10-09 14:28:04 -0500

    Adición de la función run() en el programa con condiciones if

--- fila.py
+++ fila.py
@@ -1,15 +1,20 @@
 import random as rd
 
-# espera = rd.randint(2, 3)
-# caja = rd.randint(4, 5)
-minutos = 15
-for minuto in range(minutos):
-    espera = rd.randint(2, 3)
-    caja = rd.randint(4, 5)
-    print('minuto = ' + str(minuto), end='')    
-    if (minuto >= 0) & (minuto <= espera):
-        print('\tespera = ' + str(espera), end='')
-        print('\tcaja = N/A')
-    else:
-        print('\tespera = N/A', end='')
-        print('\tcaja = ' + str(caja))
+def run():
+
+    # espera = rd.randint(2, 3)
+    # caja = rd.randint(4, 5)
+    minutos = 15
+    for minuto in range(minutos):
+        espera = rd.randint(2, 3)
+        caja = rd.randint(4, 5)
+        print('minuto = ' + str(minuto), end='')    
+        if (minuto >= 0) & (minuto <= espera):
+            print('\tespera = ' + str(espera), end='')
+            print('\tcaja = N/A')
+        else:
+            print('\tespera = N/A', end='')
+            print('\tcaja = ' + str(caja))
+
+if __name__ == '__main__':
+    run()
