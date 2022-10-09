a4c691fa05a53b03d8ba44aa55171a9a0a629e2f
Yimy Romero <jalexromeror@gmail.com>
2022-10-09 12:22:03 -0500

    Adición de números aleatorios

--- fila.py
+++ fila.py
@@ -1,7 +1,14 @@
+import random as rd
+
 minutos = 15
-espera = 3
-caja = 5
+espera = rd.randint(2, 3)
+caja = rd.randint(4, 5)
+k = 0
 for minuto in range(minutos):
+    espera = rd.randint(2, 3)
+    caja = rd.randint(4, 5)    
     print('minuto = ' + str(minuto), end='')
+    print('\tk = ' + str(k), end='')
     print('\tespera = ' + str(espera), end='')
     print('\tcaja = ' + str(caja))
+    k += 1
