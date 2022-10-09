b8e0462dbc82ecea9d6169cf89fca815da813357
Yimy Romero <jalexromeror@gmail.com>
2022-10-09 12:12:30 -0500

    Oganización del print

--- fila.py
+++ fila.py
@@ -2,5 +2,6 @@ minutos = 15
 espera = 3
 caja = 5
 for minuto in range(minutos):
-    print('minuto = ' + str(minuto) + '\tespera = ' + str(espera) 
-    + '\tcaja = ' + str(caja))
+    print('minuto = ' + str(minuto), end='')
+    print('\tespera = ' + str(espera), end='')
+    print('\tcaja = ' + str(caja))
