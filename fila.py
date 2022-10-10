9a3eb961e77120a0b6bfddf31e27cb80cf2ef5c1
Yimy Romero <jalexromeror@gmail.com>
2022-10-09 19:25:10 -0500

    Ajuste de los resultados en los print

--- fila.py
+++ fila.py
@@ -1,20 +1,21 @@
 import random as rd
 
 def run():
-
-    # espera = rd.randint(2, 3)
-    # caja = rd.randint(4, 5)
+    dias = 1
     minutos = 15
-    for minuto in range(minutos):
-        espera = rd.randint(2, 3)
-        caja = rd.randint(4, 5)
-        print('minuto = ' + str(minuto), end='')    
-        if (minuto >= 0) & (minuto <= espera):
-            print('\tespera = ' + str(espera), end='')
-            print('\tcaja = N/A')
-        else:
-            print('\tespera = N/A', end='')
-            print('\tcaja = ' + str(caja))
+    for dia in range(dias):
+        print('dia = ' + str(dia), end='')        
+        for minuto in range(minutos):
+            espera = rd.randint(2, 3)
+            caja = rd.randint(4, 5)
+            print('\tminuto = ' + str(minuto), end='')    
+            if (minuto >= 0) & (minuto <= espera):                
+                print('\tespera = ' + str(espera), end='')
+                print('')            
+            elif minuto > espera:
+                print('',end='')
+                print('\tcaja = ' + str(caja))
+
 
 if __name__ == '__main__':
     run()
